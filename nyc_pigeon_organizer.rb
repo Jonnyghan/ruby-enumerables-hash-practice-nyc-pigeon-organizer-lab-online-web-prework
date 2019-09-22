59a722255bc5dc62f428b7656bdaae46e19c218d
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-09-22 20:04:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,15 +19,19 @@ pigeon_data = {
       }
 
 def nyc_pigeon_organizer(data)
-  pigeon_list= {}
-  data.each do |info, value|
+  pigeon_list = {}
+  data.each do |color_gender_lives, value|
     value.each do |stats, all_names|
-      all_names.each do |names|
-        if pigeon_list[name]== nil 
-          pigeon_list ={}
-          binding.pry
-      
-    end
+      all_names.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][color_gender_lives] == nil
+          pigeon_list[name][color_gender_lives] = []
+        end
+        pigeon_list[name][color_gender_lives].push(stats.to_s)
+      end
+    end 
   end
- end
+  pigeon_list
 end
